e0d535fcb04d6708ffff717ba9c0b92e97e5d0bd
sb-nour <nour@simplebackups.com>
2024-05-03 16:32:12 +0300

    chore: Refactor page component to improve data fetching and rendering

--- app/page.tsx
+++ app/page.tsx
@@ -6,37 +6,35 @@ export default async function Home() {
   const data = await getData();
 
   return (
-    <main className="grid grid-cols-8 gap-20">
-      <aside className="relative col-span-2 border-r pt-10 ">
-        <menu className=" sticky top-10 space-y-2">
+    <Tabs defaultValue="json">
+      <main className="grid grid-cols-8 gap-20">
+        <aside className="relative col-span-2 border-r pt-10 ">
+          <menu className=" sticky top-10 space-y-2">
+            <TabsList>
+              <TabsTrigger value="json">JSON</TabsTrigger>
+              <TabsTrigger value="php">PHP</TabsTrigger>
+            </TabsList>
+            {Object.keys(data).map((provider) => (
+              <li className="  text-lg " key={provider}>
+                <Link href={`#${provider.toLowerCase()}`}>{provider}</Link>
+              </li>
+            ))}
+          </menu>
+        </aside>
+        <article className="prose xl:prose-xl lg:prose-lg col-span-6 w-full pt-10">
+          <h2>Cloud Provider Regions</h2>
           {Object.keys(data).map((provider) => (
-            <li className="  text-lg " key={provider}>
-              <Link href={`#${provider.toLowerCase()}`}>
-                {provider}
-              </Link>
-            </li>
-          ))}
-        </menu>
-      </aside>
-      <article className="prose xl:prose-xl lg:prose-lg col-span-6 w-full pt-10">
-        <h2>Cloud Provider Regions</h2>
-        {Object.keys(data).map((provider) => (
-          <section key={provider}>
-            <h3 id={provider.toLowerCase()}>{provider}</h3>
-            <ul>
-              {Object.keys(data[provider]).map((section) =>
-                data[provider][section] === null ? null : (
-                  <li key={section} id={section.toLowerCase()}>
-                    <h4
-                      id={`${provider.toLowerCase()}_${section.toLowerCase()}`}
-                    >
-                      {provider} {section} Regions
-                    </h4>
-                    <Tabs defaultValue="json">
-                      <TabsList>
-                        <TabsTrigger value="json">JSON</TabsTrigger>
-                        <TabsTrigger value="php">PHP</TabsTrigger>
-                      </TabsList>
+            <section key={provider}>
+              <h3 id={provider.toLowerCase()}>{provider}</h3>
+              <ul>
+                {Object.keys(data[provider]).map((section) =>
+                  data[provider][section] === null ? null : (
+                    <li key={section} id={section.toLowerCase()}>
+                      <h4
+                        id={`${provider.toLowerCase()}_${section.toLowerCase()}`}
+                      >
+                        {provider} {section} Regions
+                      </h4>
                       <TabsContent value="json">
                         <pre className="w-full">
                           {JSON.stringify(data[provider][section], null, 4)}
@@ -49,21 +47,21 @@ export default async function Home() {
                           )}
                         </pre>
                       </TabsContent>
-                    </Tabs>
-                  </li>
-                )
-              )}
-            </ul>
-          </section>
-        ))}
-      </article>
-    </main>
+                    </li>
+                  )
+                )}
+              </ul>
+            </section>
+          ))}
+        </article>
+      </main>
+    </Tabs>
   );
 }
 
 async function getData() {
   const regions = await fetch("https://providers-endpoints.vercel.app/", {
-    next: { revalidate: 600 },
+    next: { revalidate: 100 },
   });
   const data = await regions.json();
   return data;
